8cfde07c05074457aa94351682b3f99e940b0e2c
Bastiaan Quast <bquast@gmail.com>
2014-06-08 15:08:29 +0200

    finish clm graph, save data

---
+++ WesternDAC.R
@@ -0,0 +1,38 @@
+# Plotting Western Aid vs. DAC
+# Bastiaan Quast
+# bquast@gmail.com
+
+# load packages
+library(plm)
+library(plyr)
+library(ggplot2)
+library(scales)
+
+# load the data
+western.aid <- read.csv('WesternAid.csv')
+
+# inspect the data
+View(western.aid)
+
+# keep only value of Aid.type: Grants, Total
+oda <- subset(western.aid, Aid.type == 'ODA: Total Net' & Amount.type == 'Constant Prices (2012 USD millions)')
+names(oda) <- tolower(names(oda))
+
+# create a coalition of like minded dummy
+clm.list <- c('Denmark', 'Finland', 'Germany', 'Luxembourg', 'Netherlands', 'Norway', 'Sweden', 'Switzerland', 'Iceland')
+oda$clm <- ifelse(oda$donor %in% clm.list, TRUE, FALSE)
+save(oda, file = 'oda.RData' )
+
+# summarise
+oda.sum <- ddply(oda, .(clm, year), summarize, total.aid=sum(value))
+oda.sum
+
+# plot data
+plot <- ggplot(oda.sum, aes(year, total.aid, colour=clm))
+plot <- plot + geom_line() + scale_x_continuous(breaks= pretty_breaks())
+print(plot)
+
+# create image file
+png('clm.png')
+print(plot)
+dev.off()
